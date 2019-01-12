759e7c0782b734972704d2169278725651a3a57b
Scott Clarke <dawg6@users.noreply.github.com>
2019-01-12 00:08:32 -0500

    Save and load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { Data } from './data';
+import { Component, Inject } from '@angular/core';
+import { Data, Talents, Skills } from './data';
 import { FormsModule } from '@angular/forms';
+import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
+
+const SAVE_PREFIX : string = "GrowDefense.";
+const TALENTS_PREFIX : string = SAVE_PREFIX + "talents.";
+const SKILLS_PREFIX : string = SAVE_PREFIX + "skills.";
 
 @Component({
   selector: 'app-root',
@@ -10,14 +15,48 @@ import { FormsModule } from '@angular/forms';
 export class AppComponent {
   data : Data;
   Math = Math;
-  
-  constructor() {
-    this.data = new Data();
 
-    this.data.level = 135;
+  constructor(@Inject(LOCAL_STORAGE) private storage: WebStorageService) {
+    this.data = new Data();
   }
 
-  updateData(value: any) {
+  private getLocalStorage(field: string, defaultValue: string) {
+    var value: string = this.storage.get(field);
+
+    return value ? value : defaultValue;
+  }
+
+  private saveObject(prefix: string, object:any) {
+    for (var field of Object.keys(object)) {
+      this.storage.set(prefix + field, object[field]);
+    }
+  }
+
+  private loadObject(prefix: string, object:any): any {
+    for (var field of Object.keys(object)) {
+      var value = this.getLocalStorage(prefix + field, '' + object[field]);
+      console.log("Loaded " + prefix + field + "=", value);
+      object[field] = Number(value);
+    }
+
+    return object;
+  }
+
+  updateData(value?: any) {
     this.data.update();
+  }
+
+  save() {
+    this.saveObject(SKILLS_PREFIX, this.data.skills);
+    this.saveObject(TALENTS_PREFIX, this.data.talents);
+
+    alert("Form Data Saved");
+  }
+
+  load() {
+    this.loadObject(SKILLS_PREFIX, this.data.skills);
+    this.loadObject(TALENTS_PREFIX, this.data.talents);
+
+    this.updateData();
   }
 }
