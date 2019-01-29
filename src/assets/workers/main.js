7077a060fc3574bdf295559e039212ed22e5ad68
Scott Clarke <scott@dawg6.com>
2019-01-28 19:22:58 -0500

    closer to in game

--- src/assets/workers/main.js
+++ src/assets/workers/main.js
@@ -229,7 +229,7 @@ var Stats = /** @class */ (function () {
     }
     Stats.prototype.update = function (data) {
         this.arrowBase = 16 + (14 + data.power.arrow) * data.skills.arrow - data.power.arrow;
-        this.laserBase = Math.floor((12.0 + (3.0 + data.power.laser / 4.0) * data.skills.laser) * 1.3) - Math.floor(data.power.laser / 4.0);
+        this.laserBase = Math.floor((12.0 + (3.0 + data.power.laser / 4.0) * data.skills.laser) * 1.3 - (data.power.laser / 4.0));
         this.missileBase = 500 + ((data.skills.missileDamage - 1) * Math.floor(data.skills.missileDamage / 2) * (75 + (data.power.missile * 10)));
         this.fingerBase = 14 + (6 * data.skills.finger);
         this.missileROF = 3.0 - Math.round(10.0 * (data.skills.missileFiringRate * 0.1)) / 10.0;
